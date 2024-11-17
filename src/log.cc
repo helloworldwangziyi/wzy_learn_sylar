2120f9929d4fe80883a02c67b711ce5d8c15ae52
wangziyi <2389794787@qq.com>
2024-11-17 09:54:52 +0800

    修改log.cc文件，完成log模块的实现

--- src/log.cc
+++ src/log.cc
@@ -729,4 +729,46 @@ namespace sylar
         m_logger->log(m_event);
     }
 
+    LoggerManager::LoggerManager() {
+        m_root.reset(new Logger("root"));
+        m_root->addAppender(LogAppender::ptr(new StdoutLogAppender));
+        m_loggers[m_root->getName()] = m_root;
+        init();
+    }
+
+    /**
+     * 如果指定名称的日志器未找到，那会就新创建一个，但是新创建的Logger是不带Appender的，
+     * 需要手动添加Appender
+     */
+    Logger::ptr LoggerManager::getLogger(const std::string &name)
+    {
+        MutexType::Lock lock(m_mutex);
+        auto it = m_loggers.find(name);
+        if (it != m_loggers.end())
+        {
+            return it->second;
+        }
+
+        Logger::ptr logger(new Logger(name));
+        m_loggers[name] = logger;
+        return logger;
+    }
+    /**
+     * @todo 实现从配置文件加载日志配置
+     */
+    void LoggerManager::init()
+    {
+    }
+    
+    std::string LoggerManager::toYamlString() {
+        MutexType::Lock lock(m_mutex);
+        YAML::Node node;
+        for(auto& i : m_loggers) {
+            node.push_back(YAML::Load(i.second->toYamlString()));
+        }
+        std::stringstream ss;
+        ss << node;
+        return ss.str();
+    }
+
 }
